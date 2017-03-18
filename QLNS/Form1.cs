68bab57593c567209eb897d7c2ca1d796dccc40e
nguyenhienmta <nguyenhienmta2008@gmail.com>
2017-03-18 07:28:21 +0700

    first

--- QLNS/Form1.cs
+++ QLNS/Form1.cs
@@ -15,7 +15,7 @@ namespace QLNS
         public QLNS()
         {
             InitializeComponent();
-            ImageList imageList1 = new ImageList();
+           /* ImageList imageList1 = new ImageList();
             imageList1.Images.Add("key1", Image.FromFile(@"E:\STUDY\MTA\3_Third_Year\TheSecond\Thực tập nhóm\Tuan 1\New_QLNS\QLNS\QLNS\Resources\19.png"));
             imageList1.Images.Add("key2", Image.FromFile(@"E:\STUDY\MTA\3_Third_Year\TheSecond\Thực tập nhóm\Tuan 1\New_QLNS\QLNS\QLNS\Resources\20.png"));
             imageList1.Images.Add("key3", Image.FromFile(@"E:\STUDY\MTA\3_Third_Year\TheSecond\Thực tập nhóm\Tuan 1\New_QLNS\QLNS\QLNS\Resources\1.png"));
@@ -28,6 +28,7 @@ namespace QLNS
             tabControl1.TabPages.Add("tabKey1", "TabText1", "key1"); // icon using ImageKey
             tabControl1.TabPages.Add("tabKey2", "TabText2", 1);      // icon using ImageIndex
             this.Controls.Add(tabControl1);
+            */
         }
 
         private void QLNS_Load(object sender, EventArgs e)
